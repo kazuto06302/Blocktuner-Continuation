e90a9b9a3af2e50b15d019f990c2a8a7fe508b2e
kazuto06302 <galaxy0630wk@outlook.jp>
2026-09-27 01:16:08 +0900

    fix 2

--- src/main/java/net/kztmc/mc/blocktuner/mixin/InGameHudMixin.java
+++ src/main/java/net/kztmc/mc/blocktuner/mixin/InGameHudMixin.java
@@ -2,17 +2,22 @@ package net.kztmc.mc.blocktuner.mixin;
 
 import net.kztmc.mc.blocktuner.NoteNameHud;
 import net.minecraft.client.DeltaTracker;
-import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.GuiGraphicsExtractor;
+import net.minecraft.client.gui.Hud;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(Gui.class)
+@Mixin(Hud.class)
 public class InGameHudMixin {
-     @Inject(method = "extractRenderState", at = @At("TAIL"))
-     private void renderNoteNameHud(GuiGraphicsExtractor context, DeltaTracker deltaTracker, CallbackInfo ci) {
-         NoteNameHud.render(context);
-     }
+
+    @Inject(method = "extractRenderState", at = @At("TAIL"))
+    private void renderNoteNameHud(
+            GuiGraphicsExtractor context,
+            DeltaTracker deltaTracker,
+            CallbackInfo ci
+    ) {
+        NoteNameHud.render(context);
+    }
 }
